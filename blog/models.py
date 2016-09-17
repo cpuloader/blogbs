e1e79c0c91c3729418d39eea61e009ef6840cac4
cpuloader <bipsoup@gmail.com>
2016-09-17 17:26:40 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+#coding: utf-8
+from django.db import models
+from django.core.urlresolvers import reverse
+from datetime import datetime
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name=unicode('Заголовок', 'utf-8')) # заголовок поста
+    datetime = models.DateTimeField(default = datetime.now, db_index = True, verbose_name=unicode('Дата публикации', 'utf-8')) # дата публикации
+    content = models.TextField(max_length=10000, verbose_name=unicode('Основной текст', 'utf-8')) # текст поста
+
+    class Meta:
+        ordering = ["-datetime"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        #return "/blog/%i/" % self.id
+        return reverse("post_detail", kwargs = {"pk": self.pk})
---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+import json
+from django.http import HttpResponse
+
+from .models import Post
+from .forms import PostForm
+
+class PostsListView(ListView): 
+    model = Post               
+    fields = "__all__"
+    success_url = reverse_lazy("list")
+
+    def get_context_data(self, **kwargs):
+        context = super(PostsListView, self).get_context_data(**kwargs)
+        context["post_form"] = PostForm()
+        return context
+
+class PostDetailView(DetailView): 
+    model = Post
+    fields = "__all__"
+
+class PostCreateView(CreateView): 
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_add.html"
+"""
+def myscript(request):
+  output = {}
+  output["images"] = []
+  return HttpResponse(json.dumps(output), content_type = "application/json")
+"""
