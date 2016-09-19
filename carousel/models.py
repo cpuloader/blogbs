0a133191e368a401b689ca2102adb9311fc0daf9
cpuloader <bipsoup@gmail.com>
2016-09-19 09:17:42 +0300

    remove defaults in pictures

--- carousel/models.py
+++ carousel/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 from PIL import Image
 
 class Picture(models.Model):
-    title = models.CharField(max_length=200, default=None, blank=True, null=True, verbose_name=u'Заголовок')
-    text = models.TextField(default=None, blank=True, null=True, verbose_name=u'Текст описания')
+    title = models.CharField(max_length=200, verbose_name=u'Заголовок')
+    text = models.TextField(verbose_name=u'Текст описания')
     created_date = models.DateTimeField(default=timezone.now, db_index = True, verbose_name=u'Дата')
     picture = models.ImageField(verbose_name=u'Фото')
 
