812dbf23d35e51db9de1b0d9ae5ed1e4d7c504da
cpuloader <bipsoup@gmail.com>
2016-09-20 12:02:52 +0300

    login css improved

--- blog/models.py
+++ blog/models.py
@@ -15,5 +15,4 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        #return "/blog/%i/" % self.id
         return reverse("post_detail", kwargs = {"pk": self.pk})
