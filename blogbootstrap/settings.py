5aa2069d2f441ff5cdfeda4f559381dfb194c7a2
cpuloader <bipsoup@gmail.com>
2016-10-14 11:28:53 +0300

    added django-mailgun in settings

--- blogbootstrap/settings.py
+++ blogbootstrap/settings.py
@@ -129,6 +129,7 @@ SITE_ID = 1
 #EMAIL_PORT = 587
 #EMAIL_USE_TLS = True
 #DEFAULT_FROM_EMAIL = "Bols <bols@mail.ru>"
+DEFAULT_FROM_EMAIL "Mailgun Sandbox <postmaster@sandbox3debeca907c54d94bd4edc1548d5f2d3.mailgun.org>"
 EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 MAILGUN_ACCESS_KEY = 'key-8668f638ba7f7229bbc457863d303ca2'
 MAILGUN_SERVER_NAME = 'https://api.mailgun.net/v3/sandbox3debeca907c54d94bd4edc1548d5f2d3.mailgun.org/messages'
