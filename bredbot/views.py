d0bbd797a8c198a50178cd2dba87efe1ec09eb7d
cpuloader <bipsoup@gmail.com>
2017-07-04 14:12:34 +0300

    remake for python3.5

--- bredbot/views.py
+++ bredbot/views.py
@@ -16,7 +16,7 @@ from .utils import help_text, make_text, get_picture, show_smile
 if not settings.DEBUG:
   import urllib3
 
-  proxy_url = "http://proxy.server:3128"
+  proxyname = "http://proxy.server:3128"
   telepot.api._pools = {
       'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
   }
